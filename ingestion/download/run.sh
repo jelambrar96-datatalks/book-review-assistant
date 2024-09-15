d8baf7067a32c4c878f0f1e9b836fef2df692277
Jorge Lambrano <jelambrar@gmail.com>
2024-09-14 20:15:15 -0500

    add UNZIP_DATASTE env var

--- ingestion/download/run.sh
+++ ingestion/download/run.sh
@@ -10,6 +10,8 @@ BUCKET_NAME="$AWS_BUCKET_NAME"
 ACCESS_KEY="$AWS_ACCESS_KEY_ID"
 SECRET_KEY="$AWS_SECRET_ACCESS_KEY"
 
+UNZIP_FILE="$UNZIP_DATASET"
+
 # Create bucket
 # create_bucket "$BUCKET_NAME"
 mc alias set $MINIO_ALIAS $MINIO_SERVER $ACCESS_KEY $SECRET_KEY
@@ -20,9 +22,12 @@ KAGGLE_DIR='/home/kaggle/.kaggle'
 mkdir -p $KAGGLE_DIR
 echo '{"username": "${KAGGLE_USERNAME}", "key": "${KAGGLE_KEY}"}' >> $KAGGLE_DIR/.kaggle.json
 kaggle datasets download -d mohamedbakhet/amazon-books-reviews --force
-unzip -o amazon-books-reviews.zip
 
 # send files to minio
-# send_files_s3
-mc cp $CSV_FILE_1 $MINIO_ALIAS/$BUCKET_NAME/raw_dataset/amazon-books-reviews/ratings.csv
-mc cp $CSV_FILE_2 $MINIO_ALIAS/$BUCKET_NAME/raw_dataset/amazon-books-reviews/data.csv
+mc cp $DATASET_NAME.zip $MINIO_ALIAS/$BUCKET_NAME/raw_dataset/$DATASET_NAME.zip
+
+if [[ "$UNZIP_FILE" == "true" ]]; then
+    unzip -o amazon-books-reviews.zip
+    mc cp $CSV_FILE_1 $MINIO_ALIAS/$BUCKET_NAME/raw_dataset/$DATASET_NAME/ratings.csv
+    mc cp $CSV_FILE_2 $MINIO_ALIAS/$BUCKET_NAME/raw_dataset/$DATASET_NAME/data.csv
+fi
